244210ae14fa5aa91f6c33b798121ece3fc899e8
Josh <joshuawen97@gmail.com>
2021-10-06 10:54:09 +0700

    wednesday update

--- tuesday.js
+++ tuesday.js
@@ -27,6 +27,7 @@ let character = {
 character.inflictDamage();
 
 // FIZZBUZZ //////////////////////////////////////////////
+//
 function FIZZBUZZ(start, end) {
 	for (let i = start; i <= end; i++) {
 		if (i % 3 === 0 && i % 5 === 0) {
---
+++ wednesday.js
@@ -0,0 +1,21 @@
+const programmers = [];
+
+programmers[0] = 'Bill';
+programmers[1] = 'Mark';
+programmers[2] = 'Elon';
+programmers[3] = 'Alan';
+
+console.log(programmers);
+programmers.pop();
+programmers[1] = 'Alan';
+console.log(programmers);
+
+programmers.shift();
+console.log(programmers);
+// HINT HINT WINK WINK
+const myCompanies = [`Zalo`, `Vietnam Airlines`];
+const theirCompanies = [`Microsoft`, `SpaceX`];
+// concat = add two arrays together
+const techCompanies = myCompanies.concat(theirCompanies);
+console.log(techCompanies);
+// /////////////////////////////////////////////////////////////
